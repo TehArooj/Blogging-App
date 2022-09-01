f5b37a727f1a4c409492bde5a10f9c92f262f582
Tehreem Arooj <mazharm0786@gmail.com>
2022-09-01 14:17:02 +0500

    fix: loader spinner

--- src/components/LoaderSpinner.component.tsx
+++ src/components/LoaderSpinner.component.tsx
@@ -2,7 +2,7 @@ import { BallTriangle } from "react-loader-spinner";
 
 const LoaderSpinner = () => {
   return (
-    <div className="absolute top-2/4 left-2/4 right-2/4 bottom-2/4">
+    <div className="absolute top-2/4 left-2/4">
       <BallTriangle
         height={100}
         width={100}
